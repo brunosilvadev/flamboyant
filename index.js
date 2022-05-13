a2481350d8c643b1a8ad9c193f7645e6e22ece1a
Bruno Silva <brunosilva.dev@gmail.com>
2022-05-12 22:56:06 -0500

    Served html file via endpoint

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const app = require("express")();
+const path = require("path");
 
 app.listen(
     3000,
-    () => console.log("paçoca")
+    () => console.log("Application started")
 );
 
-app.get('',(req,res) => {
+app.get('/json',(req,res ) => {
     res.status(200).send(
         [{name:'Bruno', id: 1},{name:'Summer', id:3}]
     );    
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
